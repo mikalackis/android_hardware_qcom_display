63d51642a033e4dbf4d8c7ed97bc9be79bc58d83
Mikalacki Sava <mikalackis@gmail.com>
2015-04-09 15:51:44 +0200

    Bypass ArhiDroids optimizations due to compilation errors
    Change-Id: I327ad74064b884516ce2e5fd601bd077acd70270

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia libvirtual libskia
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\" -Os
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
